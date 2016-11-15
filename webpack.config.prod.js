93bc1dd75a8c0a3312a5a39cfe612a1fca20c4dd
Michael <michael.crook@intergen.co.nz>
2016-11-16 10:32:08 +1300

    started fixing hosting issues

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'docs'),
-    publicPath: '/',
+    publicPath: '/scratch-list/',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
